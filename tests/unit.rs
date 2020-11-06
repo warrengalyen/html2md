c1319e8f584d648283aa74c88606f97c9e2ef484
Warren Galyen <wgalyen@bellhelmets.com>
2020-11-06 13:22:35 -0800

    More work, start implementing handler hierarchy

--- tests/unit.rs
+++ tests/unit.rs
@@ -1,4 +1,4 @@
-extern crate html2md
+extern crate html2md;
 
 use html2md::parse;
 
@@ -9,5 +9,27 @@ fn test() {
 
 #[test]
 fn test_anchor() {
-    parse("<p><a href=\"https://mechanikadesign.com\">mumumu</a></p>");
+    parse(r#"<p><a href=\"https://mechanikadesign.com">CLOCKWORK</a></p>"#);
 }
+
+#[test]
+fn test_anchor2() {
+    parse(r#"<p><a href="https://mechanikadesign.com">CLOCKWORK</a><a href="https://www.mechanikadesign.com">STEAM</a></p>"#);
+}
+
+#[test]
+fn test_anchor3() {
+    parse(r#"<p><a href="https://mechanikadesign.com">CLOCKWORK</a><p/><a href="https://www.mechanikadesign.com">STEAM</a></p>"#);
+}
+
+
+#[test]
+fn test_image() {
+    parse(r#"<p><a href="https://example.com/over/there?name=ferret"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Ferret_2008.png/220px-Ferret_2008.png" alt="Ferret"></a><br>"#);
+}
+
+
+#[test]
+fn test_headers() {
+    parse(r#"<h1 id="example">EXAMPLE</h1><p><a href="https://www.darkhorizons.com/">Dark Horizons</a></p><h2 id="synopsis">Synopsis</h2>"#);
+} 
