2c9b832cfdad852d8554bdc3cf84386b407150a2
Warren Galyen <wgalyen@bellhelmets.com>
2020-11-10 14:14:56 -0800

    Add binary html2md

--- tests/unit.rs
+++ tests/unit.rs
@@ -1,47 +1,47 @@
 extern crate html2md;
 
-use html2md::parse;
+use html2md::parse_html;
 
 #[test]
 fn test_dumb() {
-    let md = parse("<p>AAAAAAAAAAAAA</p>");
+    let md = parse_html("<p>AAAAAAAAAAAAA</p>");
     assert_eq!(md, "AAAAAAAAAAAAA\n\n")
 }
 
 #[test]
 fn test_anchor() {
-    let md = parse(r#"<p><a href="https://mechanikadesign.com">CLOCKWORK</a></p>"#);
+    let md = parse_html(r#"<p><a href="https://mechanikadesign.com">CLOCKWORK</a></p>"#);
     assert_eq!(md, "[CLOCKWORK](https://mechanikadesign.com)\n\n")
 }
 
 #[test]
 fn test_anchor2() {
-    let md = parse(r#"<p><a href="https://mechanikadesign.com">CLOCKWORK</a><a href="https://www.mechanikadesign.com">STEAM</a></p>"#);
+    let md = parse_html(r#"<p><a href="https://mechanikadesign.com">CLOCKWORK</a><a href="https://www.mechanikadesign.com">STEAM</a></p>"#);
     assert_eq!(md, "[CLOCKWORK](https://mechanikadesign.com)[STEAM](https://www.mechanikadesign.com)\n\n")
 }
 
 #[test]
 fn test_anchor3() {
-    let md = parse(r#"<p><a href="https://mechanikadesign.com">CLOCKWORK</a><p/><a href="https://www.mechanikadesign.com">STEAM</a></p>"#);
+    let md = parse_html(r#"<p><a href="https://mechanikadesign.com">CLOCKWORK</a><p/><a href="https://www.mechanikadesign.com">STEAM</a></p>"#);
     assert_eq!(md, "[CLOCKWORK](https://mechanikadesign.com)\n\n[STEAM](https://www.mechanikadesign.com)\n\n")
 }
 
 
 #[test]
 fn test_image() {
-    let md = parse(r#"<p><a href="https://example.com/over/there?name=ferret"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Ferret_2008.png/220px-Ferret_2008.png" alt="Ferret"></a><br>"#);
+    let md = parse_html(r#"<p><a href="https://example.com/over/there?name=ferret"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Ferret_2008.png/220px-Ferret_2008.png" alt="Ferret"></a><br>"#);
     assert_eq!(md, "[![Ferret](https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Ferret_2008.png/220px-Ferret_2008.png)](https://example.com/over/there?name=ferret)\n\n")
 }
 
 #[test]
 fn test_headers() {
-    let md = parse(r#"<h1 id="example">EXAMPLE</h1><p><a href="https://www.darkhorizons.com/">Dark Horizons</a>Movie News</p><h2 id="synopsis">Synopsis</h2>"#);
+    let md = parse_html(r#"<h1 id="example">EXAMPLE</h1><p><a href="https://www.darkhorizons.com/">Dark Horizons</a>Movie News</p><h2 id="synopsis">Synopsis</h2>"#);
     assert_eq!(md, "\nEXAMPLE\n==========\n[Dark Horizons](https://www.darkhorizons.com/)Movie News\n\nSynopsis\n----------\n")
 } 
 
 #[test]
 fn test_list() {
-    let md = parse(r#"<p><ul><li>Seven things has lady Lackless</li><li>Keeps them underneath her black dress</li><li>One a thing that's not for wearing</li></ul></p>"#);
+    let md = parse_html(r#"<p><ul><li>Seven things has lady Lackless</li><li>Keeps them underneath her black dress</li><li>One a thing that's not for wearing</li></ul></p>"#);
     assert_eq!(md, r#"
 
 * Seven things has lady Lackless
@@ -54,7 +54,7 @@ fn test_list() {
 #[test]
 fn test_list_formatted() {
     // let's use some some broken html
-    let md = parse(r#"
+    let md = parse_html(r#"
     <ul><p>
         <li>You should NEVER see this error
             <ul>
@@ -70,25 +70,25 @@ fn test_list_formatted() {
     "#);
     assert_eq!(md, r#"
 
-* You should NEVER see this error 
-    * Broken lines, broken strings 
-    * Broken threads, broken springs 
-    * Broken idols, broken heads 
-    * People sleep in broken beds   
-* Ain't no use jiving 
-* Ain't no use joking 
-* EVERYTHING IS BROKEN "#)
+* You should NEVER see this error
+    * Broken lines, broken strings
+    * Broken threads, broken springs
+    * Broken idols, broken heads
+    * People sleep in broken beds
+* Ain't no use jiving
+* Ain't no use joking
+* EVERYTHING IS BROKEN"#)
 }
 
 #[test]
 fn test_quotes() {
-    let md = parse("<p><blockquote>here's a quote\n next line of it</blockquote></p>");
+    let md = parse_html("<p><blockquote>here's a quote\n next line of it</blockquote></p>");
     assert_eq!(md, "\n\n> here's a quote next line of it\n\n")
 }
 
 #[test]
 fn test_quotes2() {
-    let md = parse("<p><blockquote>here's<blockquote>nested quote!</blockquote> a quote\n next line of it</blockquote></p>");
+    let md = parse_html("<p><blockquote>here's<blockquote>nested quote!</blockquote> a quote\n next line of it</blockquote></p>");
     assert_eq!(md, r#"
 
 > here's
