43a986fac32dc27e2bfca86ea1a86af45c8e4098
Warren Galyen <wgalyen@bellhelmets.com>
2020-11-12 13:27:40 -0800

    Fix bug with anchors messing up styles

--- tests/integration.rs
+++ tests/integration.rs
@@ -1,8 +1,11 @@
 extern crate html2md;
+extern crate spectral;
 
 use html2md::parse_html;
 use std::fs::File;
 use std::io::prelude::*;
+
+use spectral::prelude::*;
 
 #[test]
 fn test_markdown() {
@@ -34,3 +37,34 @@ fn test_blank() {
     let result = parse_html(&html);
     println!("{}", result);
 }
+
+/// newlines after list shouldn't be converted into text of the last list element
+#[test]
+fn test_list_newlines() {
+    
+    let mut html = String::new();
+    let mut html_file = File::open("test-samples/blank-bug-with-list-newlines.html").unwrap();
+    html_file.read_to_string(&mut html).expect("File must be readable");
+    let result = parse_html(&html);
+    assert_that(&result).contains(".\n\nxxx xxxx");
+    assert_that(&result).contains("xx x.\n\nxxxxx:");
+}
+#[test]
+fn test_lists_from_text() {
+    let mut html = String::new();
+    let mut html_file = File::open("test-samples/blank-bug-with-lists-from-text.html").unwrap();
+    html_file.read_to_string(&mut html).expect("File must be readable");
+    let result = parse_html(&html);
+    println!("{}", result);
+}
+
+#[test]
+fn test_strong_inside_link() {
+
+
+    let mut html = String::new();
+    let mut html_file = File::open("test-samples/blank-bug-with-strong-inside-link.html").unwrap();
+    html_file.read_to_string(&mut html).expect("File must be readable");
+    let result = parse_html(&html);
+    assert_that(&result).contains("[**Just God**](http://fanfics.me/ficXXXXXXX)");
+} 
