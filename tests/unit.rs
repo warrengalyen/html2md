7c54f1f67c739adadc3816f31958e2f4e59f2b7e
Warren Galyen <wgalyen@bellhelmets.com>
2020-11-17 15:24:23 -0800

    Fix lots of bugs with tables

--- tests/unit.rs
+++ tests/unit.rs
@@ -5,19 +5,19 @@ use html2md::parse_html;
 #[test]
 fn test_dumb() {
     let md = parse_html("<p>AAAAAAAAAAAAA</p>");
-    assert_eq!(md, "AAAAAAAAAAAAA\n\n")
+    assert_eq!(md, "AAAAAAAAAAAAA")
 }
 
 #[test]
 fn test_anchor() {
     let md = parse_html(r#"<p><a href="https://mechanikadesign.com">CLOCKWORK</a></p>"#);
-    assert_eq!(md, "[CLOCKWORK](https://mechanikadesign.com)\n\n")
+    assert_eq!(md, "[CLOCKWORK](https://mechanikadesign.com)")
 }
 
 #[test]
 fn test_anchor2() {
     let md = parse_html(r#"<p><a href="https://mechanikadesign.com">CLOCKWORK</a><a href="https://www.mechanikadesign.com">STEAM</a></p>"#);
-    assert_eq!(md, "[CLOCKWORK](https://mechanikadesign.com)[STEAM](https://www.mechanikadesign.com)\n\n")
+    assert_eq!(md, "[CLOCKWORK](https://mechanikadesign.com)[STEAM](https://www.mechanikadesign.com)")
 }
 
 #[test]
@@ -26,21 +26,19 @@ fn test_anchor3() {
     assert_eq!(md, "\
 [CLOCKWORK](https://mechanikadesign.com)
 
-[STEAM](https://www.mechanikadesign.com)
-
-")
+[STEAM](https://www.mechanikadesign.com)")
 }
 
 #[test]
 fn test_escaping() {
     let md = parse_html(r#"<p>*god*'s in his **heaven** - all is right with the __world__</p>"#);
-    assert_eq!(md, "\\*god\\*\'s in his \\*\\*heaven\\*\\* \\- all is right with the \\_\\_world\\_\\_\n\n")
+    assert_eq!(md, "\\*god\\*\'s in his \\*\\*heaven\\*\\* \\- all is right with the \\_\\_world\\_\\_")
 }
 
 #[test]
 fn test_image() {
     let md = parse_html(r#"<p><a href="https://example.com/over/there?name=ferret"><img src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Ferret_2008.png/220px-Ferret_2008.png" alt="Ferret"></a><br>"#);
-    assert_eq!(md, "[![Ferret](https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Ferret_2008.png/220px-Ferret_2008.png)](https://example.com/over/there?name=ferret)  \n\n")
+    assert_eq!(md, "[![Ferret](https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Ferret_2008.png/220px-Ferret_2008.png)](https://example.com/over/there?name=ferret)")
 }
 
 #[test]
@@ -53,8 +51,7 @@ EXAMPLE
 [Dark Horizons](https://www.darkhorizons.com/)Movie News
 
 Synopsis
-----------
-")
+----------")
 }
 
 
